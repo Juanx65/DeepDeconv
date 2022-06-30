6869fcb82b60912650bef840847df671bcf2a2ab
Mauricio <maurio.aravena@sansano.usm.cl>
2022-06-30 11:47:52 -0400

    Revert Merge

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from functools import partial
 
@@ -118,6 +117,7 @@ class CallBacks:
 # bn                : batch normalization
 
 class UNet(keras.Model):
+
     def __init__(self, impulse_response, lam=0.0, f0=4, data_shape=(24, 1024, 1), blocks=4, AA=True, bn=False, dropout=0.0):
         super(UNet, self).__init__()
 
@@ -142,17 +142,8 @@ class UNet(keras.Model):
         pass
 
     def call(self, x):
-
         x_hat = self.AE(x)
-        Nch, Nt,_= self.data_shape
-        N_kernel = self.impulse_response.shape[1] #deberia ser 512
-
-        paddings = tf.constant([[0, 0], [0, 0], [N_kernel//2, 0], [0,0]])
-        x_hat_padding = tf.pad(x_hat,paddings, mode='CONSTANT')
-
-        y_hat = tf.nn.conv2d(x_hat_padding, self.impulse_response, padding="SAME", strides=1)
-        y_hat = y_hat[:,:,:Nt,:]
-
+        y_hat = tf.nn.conv2d(x_hat, self.impulse_response, padding="SAME", strides=1)#{value for value in variable}
         return x_hat, y_hat
 
     def compile(self):#, opt):
@@ -318,7 +309,7 @@ class UNet(keras.Model):
                 x = conv_wrap(x=x, filters=f)
 
         x = self.conv_layer(x, filters=1, kernel_size=kernel,
-                            use_bn=False, use_dropout=False, use_bias=True, activ="tanh")#"relu")
+                            use_bn=False, use_dropout=False, use_bias=True, activ="relu")
 
         self.AE = Model(inputs, x)
 
--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 import numpy as np
 import scipy.fft
-import scipy.signal
 from scipy.signal import windows
 import matplotlib.pyplot as plt
 import h5py
@@ -11,68 +10,112 @@ from models import CallBacks
 from random import choice
 import tensorflow as tf
 import argparse
-from scipy.io import loadmat
-from random import randint
-
 os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION']='.10'
 
 def test(opt):
-
-    """ Load DAS data """
-    annots = loadmat('data/data_deltas.mat')
-
-    data = np.array(annots["array_output"])
-    new_data = []
-    for _, dato in enumerate(data):
-        phase = randint(0,42)
-        temp_dato = np.zeros((24, 1024))
-        #primer canal
-        temp_dato[0] = dato
-        #siguientes canales
-        for ch in range(1,23):
-            dato  = fill_channel(dato, (ch+1)*phase)
-            temp_dato[ch] = dato
-        new_data.append(temp_dato)
-    new_data = np.array(new_data)
-    data = new_data
-
+    cwd = os.getcwd()
+    datadir = os.path.join(cwd, opt.data_dir)
+    kerneldir =  os.path.join(cwd, opt.kernel_dir)
+    data_file = os.path.join(datadir, "DAS_data.h5")
+    buf = 100_000
+    samp = 50.
+    """ CARGAR EL MODELO """
     """ Load impulse response """
-    kernel = np.array(annots["chirp_kernel"][0])
-    kernel = np.flip(kernel) #ya no se necesita flipiar, al parecer
+    kernel = np.load(os.path.join(kerneldir,"i_kernel.npy"))
+    # Se normaliza el kernel respecto al máximo (a diferencia de las traces DAS que se normalizan respecto a la desviación estandar)
+    kernel = kernel / kernel.max()
 
     """ Some model parameters """
-    _, Nch, Nt = data.shape
     rho = 10.0
     f0 = 8
     blocks = 3
     noise = opt.dropout
     deep_win = opt.deep_win
 
+    """ CARGAR DATA PARA PRUEBAS """
+
+    """ Load DAS data """
+    # DAS_data.h5 -> datos para leer (1.5GB) strain rate -> hay que integrarlos
+    with h5py.File(data_file, "r") as f:
+         # Nch : numero de canales, Nt = largo de muestras (1024?), SI
+        Nch, Nt = f["strainrate"].shape
+        split = int(0.45 * Nt) #incluye todo menos train data
+        data = f["strainrate"][:, split:].astype(np.float32)
+    # se normaliza cada trace respecto a su desviación estandar
+    data /= data.std()
+    Nch, Nt = data.shape
+
     """ Init Deep Learning model """
     model = UNet(
         kernel.astype(np.float32), lam=rho, f0=f0,
         data_shape=(Nch, deep_win, 1), blocks=blocks, AA=False, bn=False, dropout=noise
     )
+
     model.construct()
     model.compile()
 
     """ CARGAR PESOS AL MODELO """
     model.load_weights(str(str(Path(__file__).parent) + opt.weights)).expect_partial()#'/checkpoints/cp-0100.ckpt'))
 
-    x = data
+    """ Integrate DAS data (strain rate -> strain) """
+
+    win = windows.tukey(Nt, alpha=0.1)
+    freqs = scipy.fft.rfftfreq(Nt, d=1/samp)
+    Y = scipy.fft.rfft(win * data, axis=1)
+    Y_int = -Y / (2j * np.pi * freqs)
+    Y_int[:, 0] = 0
+    data_int = scipy.fft.irfft(Y_int, axis=1)
+    data_int /= data_int.std()
+
+    ########################################################
+
+    Nwin = data_int.shape[1] // deep_win
+    # Total number of time samples to be processed
+    Nt_deep = Nwin * deep_win #
+    #
+    data_split = np.stack(np.split(data_int[:, :Nt_deep], Nwin, axis=-1), axis=0)
+    data_split = np.stack(data_split, axis=0)
+    data_split = np.expand_dims(data_split, axis=-1)
+    # Buffer for impulses
+    batch_size = 1 # PARA TENER SOLO UN DATO EN 1 BATCH
+
+    x = np.zeros_like(data_split)
+    N = data_split.shape[0] // batch_size
+    r = data_split.shape[0] % batch_size
+    for i in range(N):
+        n_slice = slice(i * batch_size, (i + 1) * batch_size)
+        x_i = data_split[n_slice]
+        x[n_slice] = x_i
+    # If there is some residual chunk: process that too
+    if r > 0:
+        n_slice = slice((N-1 + 1) * batch_size, None)
+        x_i = data_split[n_slice]
+        x[n_slice] = x_i
 
     """ FINALMENTE HACER LA PRUEBA"""
+    good_datas = open("datos_buenos.txt","w")
+    for i in range(len(x)):
+        suma = sum(abs(x[i]))
+        suma_tot = [o[0] for o in suma]
+        suma_tot = sum(suma_tot)
+
+        if suma_tot > 25000: # para considerar datos en los que ocurra algo significativo
+            good_datas.write(str(i)+","+str(suma_tot)+"\n")
+
+    good_datas.close()
+
     i = input("index data show: ")
     while True:
         if i != "":
             image_index = int(i)
-            x_hat, y_hat = model.call(x[image_index][None,:,:])
+            x_hat, y_hat = model.call(x[image_index][None,:,:,:])
             x_hat = tf.reshape(x_hat,[24,1024])
             y_hat = tf.reshape(y_hat,[24,1024])
 
             """ GRAFICAR LOS RESULTADOS """
             samp = 80.
             t = np.arange(x_hat.shape[1]) / samp
+
 
             f, (ax1, ax2,ax3) = plt.subplots(1, 3, sharey=True)
             ax1.set_title('X Original (integrado)')
@@ -83,21 +126,18 @@ def test(opt):
             #subplot1: origina
             for i, wv in enumerate(x[image_index]):
                 ax1.plot( t, wv - 8 * i, "tab:orange")
-                #break
             plt.tight_layout()
             plt.grid()
 
             #subplot2: x_hat-> estimación de la entrada (conv kernel con la salida)
             for i, wv in enumerate(x_hat):
-                ax2.plot(t,(wv - 8 * i), "tab:red")
-                #break
+                ax2.plot(t,(15*wv - 8 * i), "tab:red")
             plt.tight_layout()
             plt.grid()
 
             #subplot3: y_hat->
             for i, wv in enumerate(y_hat):
                 ax3.plot(t,wv - 8 * i, c="k")
-                #break
             plt.tight_layout()
             plt.grid()
 
@@ -108,19 +148,6 @@ def test(opt):
         else:
             break
         i = input("index data show: ")
-
-## desfase de data para cada canal de manera circular
-## lista: dato Original
-## idx: indice a partir de donde se copia del primer dato
-def fill_channel(lista,idx):
-    channel =np.zeros(len(lista))
-
-    for i  in range(len(lista[idx:])):
-        channel[idx + i] = lista[i]
-
-
-    return channel
-
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
